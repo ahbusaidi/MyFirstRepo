d9284d3764859b53cd5d773efab148968785735a
daygoor <daygoor@gmail.com>
2017-05-09 10:24:19 +0400

    added binary file reading and counting
    Signed-off-by: daygoor <daygoor@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# MyFirstRepo
+This is a test project
+This code test the randmoness in some binary data inside a binary file!
--- main.c
+++ main.c
@@ -1,28 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-/* Nice code ^_^ */
-void removechars(){
+
+void counthex(char* filename){
 	FILE *f, *g;
-	char c;
+	unsigned long counter[256];
+	unsigned char *hex;
+	unsigned int i;
+	size_t s;
 
-	f = fopen("newcap.txt","r");
-	g = fopen("newcapout.txt","w");
+	for(i=0;i<=256;i++) counter[i]=0;
 
-	while ((c = fgetc(f)) != EOF)
-    {
-    	if(c != 0x7c)
-        fputc(c,g);
-    }
+	f = fopen(filename,"rb");
 
-    printf("-----%ld\n",ftell(f));
-    //fputc("\0",g);
+	fseek(f,0,SEEK_END);
+
+	s = ftell(f);
+
+	rewind(f);
+
+	hex = malloc(s);
+
+	fread(hex,1,s,f);
+
+	for(i=0;i<s;i++){
+		counter[hex[i]]++;
+	}
+
+	g = fopen("result.txt","w");
+	for(i=0;i<=256;i++)fprintf(g,"0x%.2x; %lu\n",i,counter[i]);
+
+	free(hex);
 	fclose(f);
-	fclose(g);	
+	fclose(g);
 }
 
-int main(){
-	printf("Welcome to file system ^_^...\n");
-	removechars();
+int main(int argc, char* argv[]){
+
+	if(argc != 2){
+		printf("error! please state file!\n");
+		printf("Usage: %s <file>\n", argv[0]);
+		return -1;
+	}
+	counthex(argv[1]);
+
 	return 0;
 }
